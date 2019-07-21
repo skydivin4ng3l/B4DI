bbc8428ab816a796b729bbf1b7bc7fa94afc3b38
blood4ng3l <blood4ng3l@hotmail.de>
2019-07-21 02:20:05 +0200

    xpBars Now working but buggy version

--- Scripts/Content/B4DI/xpBar.d
+++ Scripts/Content/B4DI/xpBar.d
@@ -23,8 +23,8 @@ instance B4DI_XpBar(B4DI_MyXpBar){
 	y = 20;
 };
 
-var int XpBar;
-var int a8_XpBar;
+
+
 
 func void B4DI_xpBar_init() {
 	// call in init_Gobal
@@ -35,21 +35,16 @@ func void B4DI_xpBar_init() {
 	HookEngine(oCNpc__OpenScreen_Status, 7 , "B4DI_xpBar_show");
 };
 
-func void B4DI_xpBar_create_a8() {
-	if(!Hlp_IsValidHandle(a8_XpBar)) {
-		a8_XpBar = Anim8_NewExt(255, Bar_SetAlpha, XpBar, false);
-	};
-};
 
-func void B4DI_xpBar_create(){
-	//preventing overlapping animations but I fear that the frameFunction still continous
-	if(Hlp_IsValidHandle(a8_XpBar)) {
-		if (!Anim8_Empty(a8_XpBar)){
-			Anim8_Delete(a8_XpBar);
-			Bar_Delete(XpBar);
-		};
-	};
-
+func int B4DI_xpBar_create(){
+	////preventing overlapping animations but I fear that the frameFunction still continous
+	//if(Hlp_IsValidHandle(a8_XpBar)) {
+	//	if (!Anim8_Empty(a8_XpBar)){
+	//		Anim8_Delete(a8_XpBar);
+	//		Bar_Delete(XpBar);
+	//	};
+	//};
+	var int XpBar;
 	if(!Hlp_IsValidHandle(XpBar)) {
 		XpBar = Bar_Create(B4DI_XpBar);
 	};
@@ -63,13 +58,13 @@ func void B4DI_xpBar_create(){
 	Bar_SetMax(XpBar, hero.exp_next);
 	Bar_SetValue(XpBar, hero.exp - exp_lastLvlUp);
 
-	B4DI_xpBar_create_a8();
+	return XpBar;
 };
 
 func void B4DI_Bar_fadeOut(var int bar) {
-
-	//Anim8_RemoveIfEmpty(a8_XpBar, true);
-	//Anim8_RemoveDataIfEmpty(a8_XpBar, true);
+	var int a8_XpBar; a8_XpBar = Anim8_NewExt(255, Bar_SetAlpha, bar, false);
+	Anim8_RemoveIfEmpty(a8_XpBar, true);
+	Anim8_RemoveDataIfEmpty(a8_XpBar, true);
 	
 	Anim8 (a8_XpBar, 255,  5000, A8_Wait);
 	Anim8q(a8_XpBar,   0, 2000, A8_SlowStart);
@@ -77,15 +72,15 @@ func void B4DI_Bar_fadeOut(var int bar) {
 };
 
 func void B4DI_xpBar_show(){
-	B4DI_xpBar_create();
-	//B4DI_Bar_fadeOut(XpBar);
-	//Bar_Show(XpBar);
-	FF_ApplyExtData(Bar_Hide, 100, 1, XpBar);
+	var int XpBar; XpBar = B4DI_xpBar_create();
+	B4DI_Bar_fadeOut(XpBar);
+	Bar_Show(XpBar);
+	
 };
 
 func void B4DI_xpBar_update() {
 	ContinueCall();
-	B4DI_xpBar_create();
+	var int XpBar; XpBar = B4DI_xpBar_create();
 	B4DI_Bar_fadeOut(XpBar);
 };
 
