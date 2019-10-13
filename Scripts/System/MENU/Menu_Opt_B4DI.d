2e71e1c05fff04053802208f03286a3cce981a3b
blood4ng3l <blood4ng3l@hotmail.de>
2019-10-14 00:12:19 +0200

    Menu update for fadein/out

--- Scripts/System/MENU/Menu_Opt_B4DI.d
+++ Scripts/System/MENU/Menu_Opt_B4DI.d
@@ -10,9 +10,14 @@ INSTANCE MENU_GAME_OPT_B4DI(C_MENU_DEF)
 				
 	items[1]		= "MENUITEM_B4DI_SCALE";
 	items[2]		= "MENUITEM_B4DI_SCALE_CHOICE";
-	
+
+	items[3]		= "MENUITEM_B4DI_BAR_FADEIN_MAX";
+	items[4]		= "MENUITEM_B4DI_BAR_FADEIN_MAX_SLIDER";
+
+	items[5]		= "MENUITEM_B4DI_BAR_FADEOUT_MIN";
+	items[6]		= "MENUITEM_B4DI_BAR_FADEOUT_MIN_SLIDER";
 				
-	items[3]		= "MENUITEM_OPT_B4DI_BACK";
+	items[7]		= "MENUITEM_OPT_B4DI_BACK";
 	
 	flags = flags | MENU_SHOW_INFO;
 };
@@ -41,8 +46,8 @@ INSTANCE MENUITEM_B4DI_SCALE(C_MENU_ITEM_DEF)
 	text[0]		=	"Bar Scale";
 	text[1]		= 	"Size of the Bars."; // Kommentar
 	// Position und Dimension	
-	posx		=	1000;	posy		=	MENU_START_Y + MENU_SOUND_DY*0;
-	dimx		=	4000;	dimy		=	750;
+	posx		=	1000;	posy		=	MENU_START_Y + MENU_DY*0;
+	dimx		=	2700;	dimy		=	600;
 	// Aktionen
 	onSelAction[0]	= SEL_ACTION_UNDEF;
 	// Weitere Eigenschaften
@@ -56,14 +61,86 @@ instance MENUITEM_B4DI_SCALE_CHOICE(C_MENU_ITEM_DEF)
 	text[0]		= 	"off|auto|50%|100%|150% | 200%";
 	fontName	=   MENU_FONT_SMALL;
 	// Position und Dimension	
-	posx		= 5000;		posy		=	MENU_START_Y + MENU_SOUND_DY*0 + MENU_CHOICE_YPLUS;
-	dimx = MENU_SLIDER_DX;	dimy 		= 	MENU_CHOICE_DY;
+	posx		= 3700;		posy		=	MENU_START_Y + MENU_DY*0 + MENU_CHOICE_YPLUS;
+	dimx = MENU_CHOICE_DX;	dimy 		= 	MENU_CHOICE_DY;
 	// Aktionen
 	onChgSetOption						= "B4DI_barScale";
 	onChgSetOptionSection 				= "B4DI";
 	// Weitere Eigenschaften
 	flags		= flags & ~IT_SELECTABLE;	
 	flags		= flags  | IT_TXT_CENTER;
+};
+
+//
+// Bar FadeIn Max
+//
+
+INSTANCE MENUITEM_B4DI_BAR_FADEIN_MAX(C_MENU_ITEM_DEF)
+{
+	backpic		=	MENU_ITEM_BACK_PIC;
+	text[0]		=	"Bar FadeIn Visibility";
+	text[1]		= 	"Max Visibility of the Bars. CAN NOT be lower than FadeOutMin "; // Kommentar
+	// Position und Dimension	
+	posx		=	1000;	posy		=	MENU_START_Y + MENU_DY*1;
+	dimx		=	2700;	dimy		=	600;
+	// Aktionen
+	onSelAction[0]	= SEL_ACTION_UNDEF;
+	// Weitere Eigenschaften
+	flags			= flags | IT_EFFECTS_NEXT;
+};
+
+instance MENUITEM_B4DI_BAR_FADEIN_MAX_SLIDER(C_MENU_ITEM_DEF)
+{
+	backPic		=	MENU_SLIDER_BACK_PIC;
+	type		=	MENU_ITEM_SLIDER;		
+	fontName	=   MENU_FONT_SMALL;
+	// Position und Dimension	
+	posx		= 3700;		posy		=	MENU_START_Y + MENU_DY*1 + MENU_SLIDER_YPLUS;
+	dimx = MENU_SLIDER_DX;	dimy 		= 	MENU_SLIDER_DY;
+	// Aktionen
+	onChgSetOption						= "B4DI_barFadeInMax";
+	onChgSetOptionSection 				= "B4DI";
+	// Weitere Eigenschaften
+	userFloat[0]	= 8; //8 Slider-Steps
+	userString[0]	= MENU_SLIDER_POS_PIC;
+
+	flags		= flags & ~IT_SELECTABLE;	
+};
+
+//
+// Bar Fadeout Min
+//
+
+INSTANCE MENUITEM_B4DI_BAR_FADEOUT_MIN(C_MENU_ITEM_DEF)
+{
+	backpic		=	MENU_ITEM_BACK_PIC;
+	text[0]		=	"Bar FadeOut Visibility";
+	text[1]		= 	"Minimal Visibility of the Bars."; // Kommentar
+	// Position und Dimension	
+	posx		=	1000;	posy		=	MENU_START_Y + MENU_DY*2;
+	dimx		=	2700;	dimy		=	600;
+	// Aktionen
+	onSelAction[0]	= SEL_ACTION_UNDEF;
+	// Weitere Eigenschaften
+	flags			= flags | IT_EFFECTS_NEXT;
+};
+
+instance MENUITEM_B4DI_BAR_FADEOUT_MIN_SLIDER(C_MENU_ITEM_DEF)
+{
+	backPic		=	MENU_SLIDER_BACK_PIC;
+	type		=	MENU_ITEM_SLIDER;		
+	fontName	=   MENU_FONT_SMALL;
+	// Position und Dimension	
+	posx		= 3700;		posy		=	MENU_START_Y + MENU_DY*2 + MENU_SLIDER_YPLUS;
+	dimx = MENU_SLIDER_DX;	dimy 		= 	MENU_SLIDER_DY;
+	// Aktionen
+	onChgSetOption						= "B4DI_barFadeOutMin";
+	onChgSetOptionSection 				= "B4DI";
+	// Weitere Eigenschaften
+	userFloat[0]	= 8; //8 Slider-Steps
+	userString[0]	= MENU_SLIDER_POS_PIC;
+
+	flags		= flags & ~IT_SELECTABLE;	
 };
 
 INSTANCE MENUITEM_OPT_B4DI_BACK(C_MENU_ITEM_DEF)
@@ -74,6 +151,7 @@ INSTANCE MENUITEM_OPT_B4DI_BACK(C_MENU_ITEM_DEF)
 	posx		=	1000;		posy		=	MENU_BACK_Y;
 	dimx		=	6192;		dimy		=	MENU_SOUND_DY;
 	// Aktionen
-	onSelAction[0]	= 	SEL_ACTION_BACK;	
+	onSelAction[0]	= 	SEL_ACTION_BACK;
+
 	flags			= flags | IT_TXT_CENTER;
 };
